d997c13827a44691e1ebfd33ff10dea807164d31
agsuvidha <67759335+agsuvidha@users.noreply.github.com>
2020-10-30 00:01:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function keyPressed()
 {
 	
 if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(ball.body,ball.body.position,{x:215,y:-260});
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:215,y:-120});
 	}
 	
 }
